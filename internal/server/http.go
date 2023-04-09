82f64339e18898a59cba3aa2e7b54f9970d97e3b
Martin Barnes <martinpb1l1l1l@gmail.com>
2023-04-09 12:56:45 +0100

    Completed server implementation

--- internal/server/http.go
+++ internal/server/http.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -17,8 +18,8 @@ func NewHTTPServer(addr string) *http.Server{
     httpsvr := newHTTPServer()
     r := mux.NewRouter()
 
-    r.handleFunc("/", httpsvr.handleConsume).Methods("GET")
-    r.handleFunc("/", httpsvr.handleProduce).Methods("POST")
+    r.HandleFunc("/", httpsvr.handleConsume).Methods("GET")
+    r.HandleFunc("/", httpsvr.handleProduce).Methods("POST")
     return &http.Server{
         Addr: addr,
         Handler: r,
@@ -29,6 +30,80 @@ type httpServer struct {
     Log *Log
 }
 
-func newHTTPServer(){
+func newHTTPServer() *httpServer {
+    return &httpServer {
+        Log: NewLog(),
+    }
+}
 
+func (s *httpServer) handleProduce(w http.ResponseWriter, r *http.Request) {
+    var req ProduceRequest
+
+    // Decode, append to log, produce output, encode into writer
+
+    err := json.NewDecoder(r.Body).Decode(&req)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    off, err := s.Log.Append(req.Record)
+    if err != nil{
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+    res := ProduceResponse {Offset: off}
+    err = json.NewDecoder(r.Body).Decode(&res)
+
+    if err != nil{
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+    
+}
+
+
+func (s *httpServer) handleConsume(w http.ResponseWriter, r *http.Request) {
+    var req ConsumeRequest 
+
+    // Decode, append to log, produce output, encode into writer
+
+    err := json.NewDecoder(r.Body).Decode(&req)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    rec, err := s.Log.Read(req.Offset)
+
+    if err == ErrOffsetNotFound {
+        http.Error(w, err.Error(), http.StatusNotFound)
+    }
+
+    if err != nil{
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+
+    res := ConsumeResponse{Record: rec}
+    err = json.NewDecoder(r.Body).Decode(&res)
+
+    if err != nil{
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+    
+}
+
+
+type ProduceRequest struct {
+    Record Record `json:"record"`
+}
+
+type ProduceResponse struct{
+    Offset uint64 `json:"offset"` 
+}
+
+type ConsumeRequest struct {
+    Offset uint64 `json:"offset"`
+}
+
+type ConsumeResponse struct{
+    Record Record `json:"record"` 
 }
