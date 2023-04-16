51391d565ff673c57dd23da3629f59416e616004
Martin Barnes <martinpb1l1l1l@gmail.com>
2023-04-16 17:48:38 +0100

    add new log test

--- internal/log/log_test.go
+++ internal/log/log_test.go
@@ -5,7 +5,9 @@ import (
 	"os"
 	"testing"
 
+	api "github.com/mpbxyz/proglog/api/v1"
 	"github.com/stretchr/testify/require"
+	//"google.golang.org/protobuf/proto"
 )
 
 func TestLog(t *testing.T)  {
@@ -13,8 +15,9 @@ func TestLog(t *testing.T)  {
         t *testing.T, log *Log,
     ) {
         "append and read a record succeeds": testAppendRead,
-    }
-    {
+        "validates out of range error happens": testOutOfRange,
+        "Init with existing segments": testInitExisting,
+    }{
         t.Run(scenario, func(t *testing.T) {
             dir, err := ioutil.TempDir("", "store-test")
             require.NoError(t, err)
@@ -29,5 +32,28 @@ func TestLog(t *testing.T)  {
 
         })
     }
+}
+
+func testAppendRead(t *testing.T, log *Log){
+    write := &api.Record {
+       Value: []byte("Hello World"), 
+    }
+
+    off, err := log.Append(write)
+    require.NoError(t, err)
+    require.Equal(t, off, uint64(0))
+
+    got, err := log.Read(off)
+    require.NoError(t, err)
+    require.Equal(t, write.Value, got.Value)
     
+}
+
+func testOutOfRange(t *testing.T, log *Log){
+    got, err := log.Read(uint64(55))
+    require.Nil(t, got)
+    require.Error(t, err)
+}
+
+func testInitExisting(t *testing.T, log *Log)  {
 }
